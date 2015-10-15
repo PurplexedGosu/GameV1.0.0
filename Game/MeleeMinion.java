1df36c7a21842150faad2110ea3be2c3a78ef9fa
heatsink <seanswally@gmail.com>
2015-10-15 14:21:31 -0700

    Latest Version of the game

---
+++ Game/MeleeMinion.java
@@ -0,0 +1,15 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class MeleeMinions here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MeleeMinion extends NPCS
+{
+    public void act() 
+    {
+       
+    }    
+}
