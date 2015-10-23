df98004374fe8ea4f370ccd1645ce205db5331b6
heatsink <seanswally@gmail.com>
2015-10-22 17:55:03 -0700

    Started cleaning comments and removed old ninja

---
+++ NPCS.java
@@ -0,0 +1,17 @@
+import greenfoot.*;
+/**
+ * Functionality in this class:
+ */
+public class NPCS extends Actor
+{
+    
+    public NPCS()
+    {
+        
+    }
+    
+    
+    public void act() 
+    {
+    }    
+}
