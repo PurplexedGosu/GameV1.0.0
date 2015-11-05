4555fc9fb9a24f28ee09914ea69cfd71124d74c9
heatsink <seanswally@gmail.com>
2015-11-04 17:56:54 -0800

    game coming along

---
+++ K3.java
@@ -0,0 +1,115 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class K3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class K3 extends Trap
+{
+    Ninja ninja;
+    Counter healthCounter = new Counter("Health: "); //HAYDENS
+    Counter shurikenCounter = new Counter("Shurikens: ");
+    Counter levelCounter = new Counter("Level: ");
+    Counter powerCounter = new Counter("Power: ");
+    int delay = 11;
+    /**
+     * Constructor for objects of class K3.
+     * 
+     */
+    public K3(Ninja ninja)
+    {
+        super();
+        this.ninja = ninja;
+        prepare();
+    }
+
+    private void prepare()
+    {
+        Door door = new Door();
+        addObject(door, 685, 50);
+
+        addObject(healthCounter, 70, 730);
+
+        addObject(shurikenCounter, 225, 730);
+        
+        addObject(powerCounter, 500, 730);
+
+        addObject(levelCounter, 355, 730);
+        addObject(ninja, 200, 200);
+        levelCounter.setValue(3);
+        addObject(ninja, 100, 100);
+        Fence fence77 = new Fence();
+        addObject(fence77, 521, 80);
+        Fence fence78 = new Fence();
+        addObject(fence78, 530, 188);
+        Fence fence79 = new Fence();
+        addObject(fence79, 538, 329);
+        fence78.setLocation(520, 188);
+        fence77.setLocation(517, 75);
+        fence79.setLocation(521, 299);
+        BlackRM blackrm = new BlackRM(2, 2);
+        addObject(blackrm, 521, 134);
+        BlackRM blackrm2 = new BlackRM(2, 2);
+        addObject(blackrm2, 526, 248);
+        Fence fence80 = new Fence();
+        addObject(fence80, 294, 275);
+        Fence fence81 = new Fence();
+        addObject(fence81, 276, 486);
+        Fence fence82 = new Fence();
+        addObject(fence82, 376, 493);
+        Fence fence83 = new Fence();
+        addObject(fence83, 492, 490);
+        fence82.setLocation(375, 487);
+        fence83.setLocation(480, 488);
+        BlackMM blackmm = new BlackMM(2, 1);
+        addObject(blackmm, 159, 492);
+        BlackMM blackmm2 = new BlackMM(2, 1);
+        addObject(blackmm2, 609, 494);
+        BlackRM blackrm3 = new BlackRM(2, 1);
+        addObject(blackrm3, 335, 617);
+        BlackRM blackrm4 = new BlackRM(2, 1);
+        addObject(blackrm4, 459, 620);
+        BlackRM blackrm5 = new BlackRM(2, 1);
+        addObject(blackrm5, 305, 394);
+        BlackRM blackrm6 = new BlackRM(2, 1);
+        addObject(blackrm6, 416, 276);
+        checkDoor();
+    }
+
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("j")&&delay>10) 
+        {
+            Menu menu = new Menu(getThisWorld());
+            Greenfoot.setWorld(menu);
+            delay = 0;
+
+        }
+        if(getObjects(Ninja.class).size() != 0)
+        {
+            healthCounter.setValue(ninja.getNINJAHP());
+            shurikenCounter.setValue(ninja.getSHURIKENNUMBER());
+            powerCounter.setValue(ninja.getPOWERBAR());
+            /**/ // TEMPORAY FUNCTIONS FOR HAYDEN TO CHANGE LEVELS TO MAKE THEM /**/ 
+
+            /**/ // TEMPORAY FUNCTIONS FOR HAYDEN TO CHANGE LEVELS TO MAKE THEM /**/ 
+        }
+    }
+
+    public void checkDoor()
+    {
+        if(ninja.checkDoor()==true)
+            Greenfoot.setWorld(new K2(ninja));
+    }
+
+    public Ninja getNinja()
+    {
+        return ninja;
+    }
+    public Trap getThisWorld()
+    {
+        return this;
+    }
+}
---
+++ Ninja.java
@@ -0,0 +1,409 @@
+import greenfoot.*;
+/**
+ * Functionality in this class:
+ * Ninja Melee
+ * Ninja Ranged
+ * Relationship to Melee AI "CURRENTLY CALLED RANGED AI"
+ */
+
+public class Ninja extends Heroes
+{
+   private int progress = 0;
+   private boolean powerUpTwo = false;
+    
+    private boolean transparent = false;
+   private int transparentDelay = 36;
+    
+    static int speedMultiplier = 2;
+    private int delay = 21;
+    // Start Hayden Variables
+    public int powerBar = 100;
+    public int powerBarDelay = 0;
+    
+    public int shurikennumber = 8;
+    
+    private int delay1 = 71;
+    private int ninjahp = 5;
+    
+    private int points = 0;
+    private int meleeDamage = 1;
+    private int rangeDamage = 1;
+    private int armor = 5;
+    // End Hayden Variables
+    
+    private int ninjaRotation = 0;
+    private int swordRotation = 0;
+    // Ninja sprite images Start [Tiffany]
+    private GreenfootImage up1 = new GreenfootImage("Ninja2/Ninja Up/Ninja Up. Left Step.png");
+    private GreenfootImage up2 = new GreenfootImage("Ninja2/Ninja Up/Ninja Up. Right Step.png");
+    
+    private GreenfootImage down1 = new GreenfootImage("Ninja2/Ninja Down/Ninja Down. Left Step.png");
+    private GreenfootImage down2 = new GreenfootImage("Ninja2/Ninja Down/Ninja Down. Right Step.png");
+    
+    private GreenfootImage left1 = new GreenfootImage("Ninja2/Ninja Left/Ninja Left. Left Step.png");
+    private GreenfootImage left2 = new GreenfootImage("Ninja2/Ninja Left/Ninja Left. Right Step.png");
+    
+    private GreenfootImage right1 = new GreenfootImage("Ninja2/Ninja Right/Ninja Right. Left Step.png");
+    private GreenfootImage right2 = new GreenfootImage("Ninja2/Ninja Right/Ninja Right. Right Step.png");
+    // Ninja sprite images End [Tiffany]
+    
+    public Ninja()
+    {
+    }
+    public void act() 
+    {
+        keyState(up1, down1, left1, right1); // Checks which movement button is pressed first
+        betaMovement(speedMultiplier, up1, up2, down1, down2, left1, left2, right1, right2); // Implements movement + animation
+        hitLightning(); // Implements melee attack
+        shootShuriken(); // Implements ranged attack
+        minionAttack();
+        damageRangedMinion();
+        transparent();
+        powerBarCount();
+        addPower();
+        addShuriken();
+        addHealth();
+        powerUpOne();
+        powerUpTwo();
+        ninjaDied();
+    }    
+    public void powerBarCount(){
+        powerBarDelay++;
+        if (powerBarDelay >= 60){
+            powerBarDelay = 0;
+            if (powerBar < 100){
+                powerBar++;
+            }
+        }
+    }
+    
+    // Melee Attack Start [Bill]
+    public void hitLightning()
+    {
+        if ((Greenfoot.isKeyDown("enter") && delay>40))
+        {
+            Lightning l1 = new Lightning(swordRotation);
+            SwordSwing swordSwing = new SwordSwing(getSwordRotation());
+            if(this.getImage()==up1||this.getImage()==up2)
+            {
+                getWorld().addObject(l1, getX(), getY()-20);
+                l1.setRotation(getRotation() -90); 
+                getWorld().addObject(swordSwing, getX(), getY() - 20);
+            }
+            else if(this.getImage()==down1||this.getImage()==down2)
+            {
+                getWorld().addObject(l1, getX(), getY()+20);
+                l1.setRotation(getRotation() -90);
+                getWorld().addObject(swordSwing, getX(), getY() + 20);
+            }
+            else if(this.getImage()==left1||this.getImage()==left2)
+            {
+                getWorld().addObject(l1, getX()-20, getY());
+                l1.setRotation(getRotation() -90);
+                swordSwing.getImage().mirrorHorizontally();
+                getWorld().addObject(swordSwing, getX() - 20, getY());
+            }
+            else if(this.getImage()==right1||this.getImage()==right2)
+            {
+                getWorld().addObject(l1, getX()+20, getY());
+                l1.setRotation(getRotation() -90);
+                getWorld().addObject(swordSwing, getX() + 20, getY());
+            }
+            delay = 0;
+        }
+        delay++;
+    }
+    // Melee Attack End [Bill]
+    // Ranged Attack Start [Sean]
+    public void shootShuriken()
+    {
+        // Up Left
+        if (Greenfoot.isKeyDown("w") && !Greenfoot.isKeyDown("s") && Greenfoot.isKeyDown("a") && !Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 225;
+            swordRotation = 0;
+        }
+        // Up Right
+        else if (Greenfoot.isKeyDown("w") && !Greenfoot.isKeyDown("s") && !Greenfoot.isKeyDown("a") && Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 315;
+            swordRotation = 0;
+        }
+        // Down Left
+        if (!Greenfoot.isKeyDown("w") && Greenfoot.isKeyDown("s") && Greenfoot.isKeyDown("a") && !Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 135;
+            swordRotation = 0;
+        }
+        // Down Right
+        else if (!Greenfoot.isKeyDown("w") && Greenfoot.isKeyDown("s") && !Greenfoot.isKeyDown("a") && Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 45;
+            swordRotation = 0;
+        }
+        // Up
+        else if (Greenfoot.isKeyDown("w") && !Greenfoot.isKeyDown("s") && !Greenfoot.isKeyDown("a") && !Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 270;
+            swordRotation = 0;
+        }
+        // Down
+        else if (!Greenfoot.isKeyDown("w") && Greenfoot.isKeyDown("s") && !Greenfoot.isKeyDown("a") && !Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 90;
+            swordRotation = 0;
+        }
+        // Left
+        else if (!Greenfoot.isKeyDown("w") && !Greenfoot.isKeyDown("s") && Greenfoot.isKeyDown("a") && !Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 180;
+            swordRotation = 90;
+        }
+        // Right
+        else if (!Greenfoot.isKeyDown("w") && !Greenfoot.isKeyDown("s") && !Greenfoot.isKeyDown("a") && Greenfoot.isKeyDown("d"))
+        {
+            ninjaRotation = 360;
+            swordRotation = 0;
+        }
+        if (Greenfoot.isKeyDown("shift")  && delay> 50 && shurikennumber >0)
+        {
+            Shuriken s1 = new Shuriken();
+            shurikennumber--;
+            // Convert to cart sqrt
+            // get x^2 + get y^2 = R
+            // add 10 to R
+            // get rotation
+            if (ninjaRotation == 225)
+            {
+                getWorld().addObject(s1, getX() - 15, getY() - 15);
+            }
+            else if (ninjaRotation == 315)
+            {
+                getWorld().addObject(s1, getX() + 15, getY() - 15);
+            }
+            else if (ninjaRotation == 135)
+            {
+                getWorld().addObject(s1, getX() - 15, getY() + 15);
+            }
+            else if (ninjaRotation == 45)
+            {
+                getWorld().addObject(s1, getX() + 15, getY() + 15);
+            }
+            else if (ninjaRotation == 270)
+            {
+                getWorld().addObject(s1, getX(), getY() - 15);
+            }
+            else if (ninjaRotation == 90)
+            {
+                getWorld().addObject(s1, getX(), getY() + 15);
+            }
+            else if (ninjaRotation == 180)
+            {
+                getWorld().addObject(s1, getX() - 15, getY());
+            }
+            else if (ninjaRotation == 360)
+            {
+                getWorld().addObject(s1, getX() + 15, getY());
+            }
+            s1.setRotation(ninjaRotation);
+            delay = 0;
+        }
+        delay++;
+    }
+    // Ranged Attack End [Sean]
+    
+    // "Currently under ranged minion" Melee minion AI Reaction Start [Hayden] 
+    public void minionAttack()
+    {
+        delay1++;
+        if (delay1 >= 35){
+            delay1 = 0;
+            NPCS actor = (NPCS) getOneIntersectingObject(RangedMinion.class);
+           if (actor != null&&!powerUpTwo)
+           {
+                ninjahp-= actor.getDamage();
+                transparentDelay = 0;
+                NinjaBlood ninjaBlood = new NinjaBlood();
+                getWorld().addObject(ninjaBlood, getX(), getY());
+           }
+           if (actor != null)
+           {
+               MinionClaw minionClaw = new MinionClaw();
+               getWorld().addObject(minionClaw, getX(), getY());
+           }
+        }
+    }
+    public void transparent()
+    {
+        if(transparentDelay<35)
+        {
+            getImage().setTransparency(100);
+        }
+        if(transparentDelay>34)
+        {
+            getImage().setTransparency(255);
+        }
+        transparentDelay++;
+    }
+    
+    public void ninjaDied()
+    {
+        if (ninjahp <= 0)
+        {
+            //Trap trapWorld = (Trap)getWorld();
+            //trapWorld.infernostart();
+            Greenfoot.setWorld(new Inferno(this));
+        }
+    }
+   
+   public int getMeleeDamage()
+    {
+        return meleeDamage;
+    }
+    
+    public int getRangeDamage()
+    {
+        return rangeDamage;
+    }
+    
+    public int getArmor()
+    {
+        return armor;
+    }
+    
+    public void setMeleeDamage(int a)
+    {
+        meleeDamage = a;
+    }
+    
+    public void setRangeDamage(int a)
+    {
+        rangeDamage = a;
+    }
+    
+    public void setArmor(int a)
+    {
+        armor = a;
+    }
+    public int getSHURIKENNUMBER(){
+        return shurikennumber;
+    }
+    public int getNINJAHP(){
+        return ninjahp;
+    }
+        public int getPOWERBAR(){
+        return powerBar;
+    }
+    public boolean checkDoor()
+    {
+        if(getWorld().getObjects(Ninja.class).size()!=0)
+        {
+        Actor actor = getOneIntersectingObject(Door.class);
+        if(actor!=null)
+            return true;
+        else 
+            return false;
+        }
+        return false;
+    }
+    public void damageRangedMinion()
+    {
+        Darkness darkness = (Darkness) getOneIntersectingObject(Darkness.class);
+        if(darkness!=null&&!powerUpTwo)
+        {
+            ninjahp-=darkness.getDamage();
+            NinjaBlood ninjaBlood = new NinjaBlood();
+            getWorld().addObject(ninjaBlood, getX(), getY());
+        }
+    }
+    // "Currently under ranged minion" Melee minion AI End [Hayden] 
+       public void powerUpOne(){
+        if (powerBar >= 1  && (canMoveUp() || canMoveDown() || canMoveLeft() || canMoveRight())){
+            
+            if (Greenfoot.isKeyDown("space"))
+            {
+                powerBar -= 1;
+                speedMultiplier = 4;
+            }
+            else
+            {
+                speedMultiplier = 2;
+            }
+        }
+        else
+        {
+            speedMultiplier = 2;
+        }
+    }
+        public void powerUpTwo(){
+            if(powerBar>0&&(Greenfoot.isKeyDown("'") || Greenfoot.isKeyDown("r")))
+            {
+                getImage().setTransparency(100);
+                powerBar-=1;
+                powerUpTwo = true;
+            }
+            else
+                powerUpTwo = false;
+    }
+        public void powerUpThree(){
+        if (powerBar >= 100){
+            
+            powerBar=0;
+        }
+    }
+        public void addShuriken(){
+        Actor SP  = getOneIntersectingObject(addShuriken.class);
+        if (SP != null){
+            shurikennumber += 3;
+            getWorld().removeObject(SP);
+        }
+    } 
+            public void addPower(){
+        Actor PP  = getOneIntersectingObject(instaPower.class);
+        if (PP != null){
+            if (powerBar <= 50)
+            {
+                powerBar += 50;
+            }
+            else
+            {
+                powerBar+= (100-powerBar);
+            }
+            
+            getWorld().removeObject(PP);
+        }
+    }
+    public void addHealth(){
+        Actor HP  = getOneIntersectingObject(HealthGlobe.class);
+        if (HP != null){
+            ninjahp += 3;
+            getWorld().removeObject(HP);
+            if(ninjahp>armor)
+                ninjahp = armor;
+        }
+    }
+    public void setPoints(int a)
+    {
+        points = a;
+    }
+    public int getPoints()
+    {
+        return points;
+    }
+    public int getHP()
+    {
+        return ninjahp;
+    }
+    public void setHP(int a)
+    {
+        ninjahp = a;
+    }
+    public void setProgress(int a)
+    {
+        progress = a;
+    }
+    public int getProgress(int a)
+    {
+        return progress;
+    }
+}
