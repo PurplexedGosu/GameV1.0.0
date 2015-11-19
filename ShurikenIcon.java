cf143d1e18ceacc1c5f66dc002cb091ac5458a78
heatsink <seanswally@gmail.com>
2015-11-19 12:29:57 -0800

    the new hope

---
+++ ShurikenIcon.java
@@ -0,0 +1,28 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class ShurikenIcon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+// http://db4sgowjqfwig.cloudfront.net/assets/258394/trefoil-shuriken.png
+// http://img.font.downloadatoz.com/download/imgs/d/i/a/diablo-heavy-lower.png
+
+public class ShurikenIcon extends Clickables
+{
+     private GreenfootImage box = new GreenfootImage(55,55);
+    /**
+     * Act - do whatever the ShurikenIcon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public ShurikenIcon()
+    {
+        
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ Trap.java
@@ -0,0 +1,40 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Trap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Trap extends World
+{
+    Ninja ninja;
+    Boy boy;
+    DeathWorld deathWorld;
+    /**
+     * Constructor for objects of class Trap.
+     * 
+     */
+    public Trap()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(900, 750, 1); 
+    }
+    public Ninja getNinja()
+    {
+        return ninja;
+    }
+    public Boy getBoy()
+    {
+        return boy;
+    }
+    public void infernostart()  // [Hayden]
+   {
+        deathWorld = new DeathWorld(ninja);
+        Greenfoot.setWorld(deathWorld);
+   }
+      public void gameover(){
+       ninja.setHP(ninja.getArmor());
+       Greenfoot.setWorld(new Kyobashi(ninja));
+    }
+}
